96910c49035375509ada40d39c8ad1a92b85025e
Riley Johnston <rileydanejohnston@gmail.com>
2022-01-21 20:34:08 -0600

    feat: add toBe tests

---
+++ methods/toBe/toBe.test.js
@@ -0,0 +1,32 @@
+test('test same string equals same string', () => {
+  expect('string').toBe('string');
+})
+
+test('1 greater than 0 to be true', () => {
+  expect(1 > 0).toBe(true);
+})
+
+test('1 greater than 1 to be false', () => {
+  expect(1 > 1).toBe(false);
+})
+
+test('1 === 1 to be false', () => {
+  expect('1' === 1).toBe(false);
+})
+
+test('1 == 1 to be true', () => {
+  expect('1' == 1).toBe(true);
+})
+
+const a = {};
+const b = {};
+
+// test passes
+test('object reference to SAME object to be true', () => {
+  expect(a).toBe(a);
+})
+
+// test fails
+test('object reference to DIFFERENT object to be false', () => {
+  expect(a).toBe(b);
+})
